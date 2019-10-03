c86b4e16a3785f2f8550b06c9c916293edee1b2c
Tom Gorham <tsgorham@sympatico.ca>
2019-10-03 18:59:05 -0400

    initial loading

---
+++ Assign_2.py
@@ -0,0 +1,26 @@
+"""
+Envelope  print
+"""
+
+BORDER = f'+{"":->50}+'
+STAMP = f'|{"#### |":>51}'
+SPACER = f'|{"|":>51}'
+
+EDGE = f'{"|":20}'
+
+ADDRESS_1 = 'George Costanza'
+ADDRESS_2 = '1234 Fake St'
+ADDRESS_3 = 'Del Boca Vista, FL, 90210'
+
+print(BORDER)
+print(STAMP)
+print(STAMP)
+print(STAMP)
+print(SPACER)
+
+print(f'{EDGE}{ADDRESS_1:<31}|')
+print(f'{EDGE}{ADDRESS_2:<31}|')
+print(f'{EDGE}{ADDRESS_3:<31}|')
+
+print(SPACER)
+print(BORDER)
---
+++ README.md
@@ -0,0 +1,12 @@
+# Assignment Requirements
+
+Create:
+* a public repository
+* A README.md describing the project and instructions on how to use it (i.e. brief documentation)
+* A .gitignore file suited for Python projects
+* A working Python script
+
+# Submission
+
+Submit the URL of your Github repo.
+***
